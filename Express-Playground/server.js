6b4c35298439610063bc0a468f3ac20fa8e3b9d6
Michael R <mrwolf.lmriii@gmail.com>
2022-03-14 19:41:27 -0700

    Express server running

--- Express-Playground/server.js
+++ Express-Playground/server.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = '4000';
 
 // express app running under the name app
 const app = express();
+// uses morgan middleware. 'dev' gives more data for devs
+app.use(morgan('dev'));
+
+// this let's express serve files from public folder
+// __dirname special node variable refers to absolute path of current directory of the file that's in
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res) =>{
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
