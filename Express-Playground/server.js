a8a68844221bad6336a7ae4537df2f90ab25f4e2
Michael R <mrwolf.lmriii@gmail.com>
2022-03-14 19:33:03 -0700

    Playing with Express

---
+++ Express-Playground/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const hostname = 'localhost';
+const port = '4000';
+
+// express app running under the name app
+const app = express();
+
+app.use((req, res) =>{
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
+
+
+
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
